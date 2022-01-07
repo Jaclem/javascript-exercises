a0fce8dcf2e21c99fd8c2ada4172e56b77af61ea
Joshua Clement <jaclem12@gmail.com>
2022-01-07 18:44:30 -0500

    Finished Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // takes the string and turns it to lowercase and gets rid of all punctuation
+    let first = string.toLowerCase().replace(/[^0-9a-zA-Z]/g, '');
+
+    // takes the string and reverses it and removes all punctuation
+    let diff = string.split('').reverse().join('');
+    let strReplace = diff.toLowerCase().replace(/[^0-9a-zA-Z]/g, '');
+
+    // checks if the reversed string is the same or different from the original
+    if(strReplace != first){
+        return false;
+    } else {
+        return true;
+    }
 };
+
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
